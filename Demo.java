3e843db6187a3a655bf7ac5aa7709a6471d6db13
pthom2503 <pthom2503@gmail.com>
2017-06-13 11:02:36 -0500

    Add files via upload
    Working sim totally in Eclipse

---
+++ Demo.java
@@ -0,0 +1,109 @@
+package demoPack;
+import java.util.Scanner;
+
+public class Demo {
+
+	//number of iterations to run
+final static int COUNT = 5000;	
+	
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		//all vars currently constant for testing
+		double top = 100;
+		double bot = 0;
+		double left = 75;
+		double right = 50;		
+		int d = 3;
+		
+		double[][] oldPlate = new double[d+2][d+2];
+		double[][] newPlate = new double[d+2][d+2];
+		//sets edge temperatures for plates
+		oldPlate = initialize(oldPlate, top, bot, left, right);
+		newPlate = initialize(newPlate, top, bot, left, right);
+		//shows initial edge temperatures
+		for(int i = 0; i <= d+1; i++){
+			for(int j = 0; j <= d+1; j++){
+				System.out.print(newPlate[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		//runs simulation
+		iterator(oldPlate, newPlate);
+		
+	}
+	
+	
+	//takes the double 2d arrays old and new and iterates through the temperature change
+	//prints final array of heat distribution
+	public static void iterator(double[][] oldPlate, double[][] newPlate){
+	int runs = 0;
+	int d = oldPlate.length - 2;
+	//while the wanted num of iterations hasn't been reached
+	while (! done(runs) ){
+		//for each point on the plate, average surrounding temps for new temp
+		for(int i = 1; i <= d; i++){
+			for(int j = 1; j <= d; j++){
+				newPlate[i][j] = (oldPlate[i+1][j] + oldPlate[i-1][j]
+						+ oldPlate[i][j+1] + oldPlate[i][j-1])/4.0;
+			}
+		}
+	runs++;
+	//change new iteration of temps to the base (old)
+	oldPlate = swap(oldPlate, newPlate);
+	}
+	
+	for(int i = 1; i <= d; i++){
+		for(int j = 1; j <= d; j++){
+			System.out.print(newPlate[i][j] + "\t");
+		}
+		System.out.println();
+	}
+	System.out.println();
+	
+	}
+	
+	
+	
+	
+	//takes the 2D double array plate and double values and returns the 2D array
+	//with edge values in place
+	public static double[][] initialize(double[][] plate, double top, double bot, double left,
+			double right){
+		for(int i = 0; i < plate.length; i++){
+		plate[0][i]=top;
+		plate[plate.length-1][i]=bot;
+		plate[i][0]=left;
+		plate[i][plate.length-1]=right;
+		}
+		plate[0][0]=(top+left)/2.0;
+		plate[0][plate.length-1]=(top+right)/2.0;
+		plate[plate.length-1][0]=(bot+left)/2.0;
+		plate[plate.length-1][plate.length-1]=(bot+right)/2.0;
+		return plate;
+		
+	}
+	
+	
+	//takes the number of runs completed and returns true if equal to the number of runs wanted
+	public static boolean done(int runs){
+		boolean isDone = false;
+		if (runs == COUNT){
+			isDone = true;
+		}
+		return isDone;
+	}
+
+	
+	//takes 2 double arrays and replaces the value of "old" with the current values in "new"
+	public static double[][] swap(double[][] oldPlate, double[][]newPlate){
+		for(int i = 0; i < oldPlate.length; i++){
+			for(int j = 0; j < oldPlate.length; j++){
+				oldPlate[i][j] = newPlate[i][j];
+			}
+		}
+		
+		return(oldPlate);
+	}
+	
+}
