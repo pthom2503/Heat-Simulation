6b4ee5c246baefd0115ffa3529ace2aa5b816a64
pthom2503 <pthom2503@gmail.com>
2017-06-15 10:35:21 -0500

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Demo {
 
 	// number of iterations to run
-	static int COUNT = 5000;
+	static int COUNT = 0;
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -16,42 +16,39 @@ public class Demo {
 		double left = 0;
 		double right = 0;
 		int d = 3;
-		int x = 0, y;
+		int x = 0;
 		String arg;
 
+		// processes each of the flags and assigns the value after them to the
+		// corresponding variable
+		// if a value after a flag is missing, the resulting error will be of
+		// type mismatch as it tries to cast the next flag to the value
+		// or, if at end, out of bounds
 		while (x < args.length && args[x].startsWith("-")) {
 			arg = args[x++];
 			switch (arg) {
 			case "-d":
-				System.out.println("Option d");
-				d = Integer.parseInt(args[x]);
+				d = Integer.parseInt(args[x++]);
 				break;
 			case "-t":
-				System.out.println("Option t");
-				top = Double.parseDouble(args[x]);
+				top = Double.parseDouble(args[x++]);
 				break;
 			case "-b":
-				System.out.println("Option b");
-				bot = Double.parseDouble(args[x]);
+				bot = Double.parseDouble(args[x++]);
 				break;
 			case "-l":
-				System.out.println("Option l");
-				left = Double.parseDouble(args[x]);
+				left = Double.parseDouble(args[x++]);
 				break;
 			case "-r":
-				System.out.println("Option r");
-				right = Double.parseDouble(args[x]);
+				right = Double.parseDouble(args[x++]);
 				break;
-			case "-i":
-				System.out.println("Option i");
-				COUNT = Integer.parseInt(args[x]);
-			break;
+			case "-c":
+				COUNT = Integer.parseInt(args[x++]);
+				break;
 			default:
 				System.err.println("ParseCmdLine: illegal option " + arg);
 				break;
 			}
-			x++;
-
 		}
 
 		double[][] oldPlate = new double[d + 2][d + 2];
@@ -62,7 +59,7 @@ public class Demo {
 		// shows initial edge temperatures
 		for (int i = 0; i <= d + 1; i++) {
 			for (int j = 0; j <= d + 1; j++) {
-				System.out.print(newPlate[i][j] + "\t");
+				System.out.print(oldPlate[i][j] + "\t");
 			}
 			System.out.println();
 		}
@@ -78,10 +75,15 @@ public class Demo {
 	public static void iterator(double[][] oldPlate, double[][] newPlate) {
 		int runs = 0;
 		int d = oldPlate.length - 2;
-		// while the wanted num of iterations hasn't been reached
-		while (!done(runs)) {
-			// for each point on the plate, average surrounding temps for new
-			// temp
+		double[][] oldPlateCopy = new double[oldPlate.length][oldPlate.length];
+		for (int k = 0; k < oldPlate.length; k++) {
+			for (int m = 0; m < oldPlate.length; m++) {
+				oldPlateCopy[k][m] = oldPlate[k][m];
+			}
+		}
+		// while the wanted num of iterations hasn't been reached, if a user entered a number OR temp isn't stable
+		while (!done(runs, oldPlateCopy, newPlate)) {
+			// for each point on the plate, average surrounding temps for new temp
 			for (int i = 1; i <= d; i++) {
 				for (int j = 1; j <= d; j++) {
 					newPlate[i][j] = (oldPlate[i + 1][j] + oldPlate[i - 1][j] + oldPlate[i][j + 1] + oldPlate[i][j - 1])
@@ -90,6 +92,11 @@ public class Demo {
 			}
 			runs++;
 			// change new iteration of temps to the base (old)
+			for (int k = 0; k < oldPlate.length; k++) {
+				for (int m = 0; m < oldPlate.length; m++) {
+					oldPlateCopy[k][m] = oldPlate[k][m];
+				}
+			}
 			oldPlate = swap(oldPlate, newPlate);
 		}
 
@@ -123,10 +130,23 @@ public class Demo {
 
 	// takes the number of runs completed and returns true if equal to the
 	// number of runs wanted
-	public static boolean done(int runs) {
-		boolean isDone = false;
-		if (runs == COUNT) {
-			isDone = true;
+	public static boolean done(int runs, double[][] older, double[][] newer) {
+		boolean isDone = true;
+		if (COUNT != 0) {
+			if (runs != COUNT) {
+				isDone = false;
+				return isDone;
+			}
+		} else if (runs <= 1) {
+			isDone = false;
+			return isDone;
+		} else {
+			for (int k = 0; k < older.length; k++) {
+				for (int m = 0; m < older.length; m++) {
+					if (older[k][m] != newer[k][m])
+						isDone = false;
+				}
+			}
 		}
 		return isDone;
 	}
