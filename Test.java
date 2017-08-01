a3b4dbf2f20b1abf5834d036f6cef59fa8bd221a
pthom2503 <pthom2503@gmail.com>
2017-08-01 16:57:41 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -5,49 +5,61 @@ import java.math.RoundingMode;
 import javax.swing.*;
 import demoPack.TempAdjust;
 
-
-public class Test extends TempAdjust implements ActionListener {
+public class Test extends TempAdjust implements ActionListener, ItemListener {
 	// Private variables of the GUI components
-	JTextField tField, bField, lField, rField, dimRowField, dimColField, countField;
+	JTextField tField, bField, lField, rField, dimRowField, dimColField, countField, countFieldCent;
 	JButton runButton;
 	int dRow, dCol, count;
 	String defaultTemp = "0";
+	boolean center = false;
+	BigDecimal[][] finalPlate;
+	JPanel cards; // a panel that uses CardLayout
+	final static String EDGEPANEL = "Card with Edge Heat";
+	final static String CENTPANEL = "Card with Center Heat";
 
-	// Constructor to set up all the GUI components
-	public Test() {
+	public void addComponentToPane(Container pane) {
+		// Put the JComboBox in a JPanel to get a nicer look.
+		JPanel comboBoxPane = new JPanel(); // use FlowLayout
+		String comboBoxItems[] = { EDGEPANEL, CENTPANEL };
+		JComboBox cb = new JComboBox(comboBoxItems);
+		cb.setEditable(false);
+		cb.addItemListener(this);
+		comboBoxPane.add(cb);
+
+		// Create the "cards".
 		// JPanel for the text fields
-		JPanel tfPanel = new JPanel(new GridLayout(8, 2, 10, 2));
-		tfPanel.setBorder(BorderFactory.createTitledBorder("Input Plate Information: "));
+		JPanel sidesPanel = new JPanel(new GridLayout(8, 2, 10, 2));
+		sidesPanel.setBorder(BorderFactory.createTitledBorder("Input Plate Information: "));
 
 		// Top temp (Row 1)
-		tfPanel.add(new JLabel("  Top Temperature: "));
+		sidesPanel.add(new JLabel("  Top Temperature: "));
 		tField = new JTextField(10);
-		tfPanel.add(tField);
+		sidesPanel.add(tField);
 
 		// Bot temp (Row 2)
-		tfPanel.add(new JLabel("  Bottom Temperature: "));
+		sidesPanel.add(new JLabel("  Bottom Temperature: "));
 		bField = new JTextField(10);
-		tfPanel.add(bField);
+		sidesPanel.add(bField);
 
 		// Left temp (Row 3)
-		tfPanel.add(new JLabel("  Left Temperature: "));
+		sidesPanel.add(new JLabel("  Left Temperature: "));
 		lField = new JTextField(10);
-		tfPanel.add(lField);
+		sidesPanel.add(lField);
 
 		// Right temp (Row 4)
-		tfPanel.add(new JLabel("  Right Temperature: "));
+		sidesPanel.add(new JLabel("  Right Temperature: "));
 		rField = new JTextField(10);
-		tfPanel.add(rField);
+		sidesPanel.add(rField);
 
 		// Rows (Row 5)
-		tfPanel.add(new JLabel("  Rows: "));
+		sidesPanel.add(new JLabel("  Rows: "));
 		dimRowField = new JTextField(10);
-		tfPanel.add(dimRowField);
-		
+		sidesPanel.add(dimRowField);
+
 		// Columns (Row 6)
-				tfPanel.add(new JLabel("  Columns: "));
-				dimColField = new JTextField(10);
-				tfPanel.add(dimColField);
+		sidesPanel.add(new JLabel("  Columns: "));
+		dimColField = new JTextField(10);
+		sidesPanel.add(dimColField);
 
 		// Number of runs to do (Row 7)
 		// if checkBox is enabled, will use number of runs given, else will run
@@ -70,19 +82,110 @@ public class Test extends TempAdjust implements ActionListener {
 			}
 		});
 
-		tfPanel.add(countCheckBox);
-		tfPanel.add(countField);
+		sidesPanel.add(countCheckBox);
+		sidesPanel.add(countField);
 
 		// button to run simulation with given data
 		runButton = new JButton("Run");
-		tfPanel.add(runButton);
+		sidesPanel.add(runButton);
 		// if the actionListener is activated, opens a new JFrame with the
 		// resulting plate with heat distribution
-		runButton.addActionListener(new ActionListener() {
+		runButton.addActionListener(this);
+
+
+		JPanel midPanel = new JPanel(new GridLayout(5, 2, 10, 2));
+		sidesPanel.setBorder(BorderFactory.createTitledBorder("Input Plate Information: "));
+
+		// Top temp (Row 1)
+		midPanel.add(new JLabel("Temperature: "));
+		tField = new JTextField(10);
+		midPanel.add(tField);
+
+		// Rows (Row 2)
+		midPanel.add(new JLabel("  Rows: "));
+		dimRowField = new JTextField(10);
+		midPanel.add(dimRowField);
+
+		// Columns (Row 3)
+		midPanel.add(new JLabel("  Columns: "));
+		dimColField = new JTextField(10);
+		midPanel.add(dimColField);
+
+		// Number of runs to do (Row 4)
+		// if checkBox is enabled, will use number of runs given, else will run
+		// until temp stabilizes
+		countFieldCent = new JTextField(10);
+		JCheckBox checkBox = new JCheckBox("Use set number of runs", false);
+		checkBox.addItemListener(new ItemListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				String sTop, sBot, sLeft, sRight;
-				BigDecimal top, bot, left, right;
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					countFieldCent.setEnabled(true);
+
+				} else if (e.getStateChange() == ItemEvent.DESELECTED) {
+					countFieldCent.setEnabled(false);
+					countFieldCent.setText("");
+				}
+
+				validate();
+				repaint();
+			}
+		});
+
+		midPanel.add(checkBox);
+		midPanel.add(countFieldCent);
+
+		// button to run simulation with given data
+		runButton = new JButton("Run");
+		midPanel.add(runButton);
+		// if the actionListener is activated, opens a new JFrame with the
+		// resulting plate with heat distribution
+		runButton.addActionListener(this);
+
+		// Create the panel that contains the "cards".
+		cards = new JPanel(new CardLayout());
+		cards.add(sidesPanel, EDGEPANEL);
+
+		cards.add(midPanel, CENTPANEL);
+
+		pane.add(comboBoxPane, BorderLayout.PAGE_START);
+		pane.add(cards, BorderLayout.CENTER);
+	}
+
+	/**
+	 * Create the GUI and show it. For thread safety, this method should be
+	 * invoked from the event dispatch thread.
+	 */
+	private static void createAndShowGUI() {
+		// Create and set up the window.
+		JFrame frame = new JFrame("CardLayoutDemo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Create and set up the content pane.
+		Test demo = new Test();
+		demo.addComponentToPane(frame.getContentPane());
+		// Display the window.
+		frame.setTitle("Heat Distribution Simultation");
+		frame.setSize(450, 350);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	/** The entry main() method */
+	public static void main(String[] args) {
+		// Run GUI codes in Event-Dispatching thread for thread safety
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				createAndShowGUI(); // Let the constructor do the job
+			}
+		});
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+				String sTop="", sBot="", sLeft="", sRight="";
+				BigDecimal top, bot = null, left=null, right=null;
 				int red;
 
 				// adds default case in case the blanks are left empty
@@ -90,7 +193,11 @@ public class Test extends TempAdjust implements ActionListener {
 					sTop = tField.getText();
 				} else
 					sTop = defaultTemp;
+				
+				if(!center){
+					
 				if (!bField.getText().equals("")) {
+				
 					sBot = bField.getText();
 				} else
 					sBot = defaultTemp;
@@ -103,142 +210,138 @@ public class Test extends TempAdjust implements ActionListener {
 					sRight = rField.getText();
 				} else
 					sRight = defaultTemp;
-
-				if (!dimRowField.getText().equals("")) {
-					try {
-						dRow = Integer.parseInt(dimRowField.getText());
-					} catch (Exception ex) {
-						dRow = 3;
-					}
-				} else
-					dRow = 3;
-
-				if (!dimColField.getText().equals("")) {
-					try {
-						dCol = Integer.parseInt(dimColField.getText());
-					} catch (Exception ex) {
-						dCol = 3;
-					}
-				} else
-					dCol = 3;
-				
-				if (!countField.getText().equals("")) {
-					try {
-						count = Integer.parseInt(countField.getText());
-					} catch (Exception ex) {
-						count = 0;
-					}
-				} else
-					count = 0;
-
-				// verifies that all entered values are valid options, then
-				// assigns value to the exterior temp variables
-				try {
-					top = new BigDecimal(sTop);
-				} catch (Exception ex) {
-					sTop = defaultTemp;
-					top = new BigDecimal(sTop);
-				}
-
-				try {
-					bot = new BigDecimal(sBot);
-				} catch (Exception ex) {
-					sBot = defaultTemp;
-					bot = new BigDecimal(sBot);
-				}
-
-				try {
-					left = new BigDecimal(sLeft);
-				} catch (Exception ex) {
-					sLeft = defaultTemp;
-					left = new BigDecimal(sLeft);
-				}
-				try {
-					right = new BigDecimal(sRight);
-				} catch (Exception ex) {
-					sLeft = defaultTemp;
-					right = new BigDecimal(sRight);
-				}
-
-				// sets the precision to 2 decimal places and rounding to
-				// normal rounding
-				top = top.setScale(2, RoundingMode.HALF_UP);
-				bot = bot.setScale(2, RoundingMode.HALF_UP);
-				left = left.setScale(2, RoundingMode.HALF_UP);
-				right = right.setScale(2, RoundingMode.HALF_UP);
-
-				// creates the plates
-				BigDecimal[][] oldPlate = new BigDecimal[dRow + 2][dCol + 2];
-				BigDecimal[][] newPlate = new BigDecimal[dRow + 2][dCol + 2];
-				// sets edge temperatures for plates
-				oldPlate = initialize(oldPlate, top, bot, left, right);
-				BigDecimal[][] finalPlate = new BigDecimal[dRow + 2][dCol + 2];
-				// makes a final plate for display purposes
-				finalPlate = initialize(newPlate, top, bot, left, right);
-	
-				finalPlate = iterator(oldPlate, newPlate, count);
-
-				// final display frame
-				JFrame frame = new JFrame("Heat Distribution");
-				
-				JPanel grid = new JPanel();
-				grid.setLayout(new GridLayout(dRow, dCol));
-				for (int i = 1; i <= dRow; i++) {
-					for (int j = 1; j <= dCol; j++) {
-						JLabel pointData = new JLabel(finalPlate[i][j] + "\t");
-						
-						//sets the color gradient of the text based on the temperature
-						red = (int) (finalPlate[i][j].setScale(0, RoundingMode.DOWN).intValueExact()*2.55);
-						
-						//ensures the red value is within acceptable range
-						if(red>255){
-							red=255;
-						}
-						if(red<0){
-							red=0;
-						}
-						
-						pointData.setForeground(new Color(red, 0, 0));
-						grid.add(pointData);
-					}
-					frame.setContentPane(grid);
-					frame.getContentPane().setBackground(Color.lightGray);
-					frame.pack();
-					frame.setVisible(true);
-
-				}
-			}
-
-		});
-
-		// Setup the content-pane of JFrame in BorderLayout
-		Container cp = this.getContentPane();
-		cp.setLayout(new BorderLayout(5, 5));
-		
-		
-		cp.add(tfPanel, BorderLayout.NORTH);
-
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Heat Distribution Simultation");
-		setSize(450, 350);
-		setVisible(true);
 	}
 
-	/** The entry main() method */
-	public static void main(String[] args) {
-		// Run GUI codes in Event-Dispatching thread for thread safety
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new Test(); // Let the constructor do the job
+	if(!dimRowField.getText().equals(""))
+
+	{
+		try {
+			dRow = Integer.parseInt(dimRowField.getText());
+		} catch (Exception ex) {
+			dRow = 3;
+		}
+	}else dRow=3;
+
+	if(!dimColField.getText().equals(""))
+	{
+		try {
+			dCol = Integer.parseInt(dimColField.getText());
+		} catch (Exception ex) {
+			dCol = 3;
+		}
+	}else dCol=3;
+
+	if(!countField.getText().equals(""))
+	{
+		try {
+			count = Integer.parseInt(countField.getText());
+		} catch (Exception ex) {
+			count = 0;
+		}
+	}else count=0;
+
+	// verifies that all entered values are valid options, then
+	// assigns value to the exterior temp variables
+	try
+	{
+		top = new BigDecimal(sTop);
+	}catch(
+	Exception ex)
+	{
+		sTop = defaultTemp;
+		top = new BigDecimal(sTop);
+	}
+if (!center){
+	try
+	{
+		bot = new BigDecimal(sBot);
+	}catch(
+	Exception ex)
+	{
+		sBot = defaultTemp;
+		bot = new BigDecimal(sBot);
+	}
+
+	try
+	{
+		left = new BigDecimal(sLeft);
+	}catch(
+	Exception ex)
+	{
+		sLeft = defaultTemp;
+		left = new BigDecimal(sLeft);
+	}try
+	{
+		right = new BigDecimal(sRight);
+	}catch(
+	Exception ex)
+	{
+		sLeft = defaultTemp;
+		right = new BigDecimal(sRight);
+	}
+}
+	// sets the precision to 2 decimal places and rounding to
+	// normal rounding
+	top=top.setScale(2,RoundingMode.HALF_UP);bot=bot.setScale(2,RoundingMode.HALF_UP);left=left.setScale(2,RoundingMode.HALF_UP);right=right.setScale(2,RoundingMode.HALF_UP);
+
+	// makes a final plate for display purposes
+
+	if(center)
+	{
+		// creates the plates
+		BigDecimal[][] oldPlateCenter = new BigDecimal[dRow][dCol];
+		BigDecimal[][] newPlateCenter = new BigDecimal[dRow][dCol];
+		// sets edge temperatures for plates
+		oldPlateCenter = initialize(oldPlateCenter, top, bot, left, right);
+		finalPlate = new BigDecimal[dRow][dCol];
+		finalPlate = initialize(newPlateCenter, top, bot, left, right);
+	}else
+	{
+		// creates the plates
+		BigDecimal[][] oldPlateSides = new BigDecimal[dRow + 2][dCol + 2];
+		BigDecimal[][] newPlateSides = new BigDecimal[dRow + 2][dCol + 2];
+		// sets edge temperatures for plates
+		oldPlateSides = initialize(oldPlateSides, top, bot, left, right);
+		finalPlate = new BigDecimal[dRow + 2][dCol + 2];
+		finalPlate = initialize(newPlateSides, top, bot, left, right);
+		finalPlate = iterator(oldPlateSides, newPlateSides, count);
+	}
+
+	// final display frame
+	JFrame frame = new JFrame("Heat Distribution");
+
+	JPanel grid = new JPanel();grid.setLayout(new GridLayout(dRow,dCol));for(
+	int i = 1;i<=dRow;i++)
+	{
+		for (int j = 1; j <= dCol; j++) {
+			JLabel pointData = new JLabel(finalPlate[i][j] + "\t");
+
+			// sets the color gradient of the text based on the temperature
+			red = (int) (finalPlate[i][j].setScale(0, RoundingMode.DOWN).intValueExact() * 2.55);
+
+			// ensures the red value is within acceptable range
+			if (red > 255) {
+				red = 255;
 			}
-		});
+			if (red < 0) {
+				red = 0;
+			}
+
+			pointData.setForeground(new Color(red, 0, 0));
+			grid.add(pointData);
+		}
+		frame.setContentPane(grid);
+		frame.getContentPane().setBackground(Color.lightGray);
+		frame.pack();
+		frame.setVisible(true);
+
+	}
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+	public void itemStateChanged(ItemEvent evt) {
+        CardLayout cl = (CardLayout)(cards.getLayout());
+        cl.show(cards, (String)evt.getItem());
+    }
 
 }
